7e69f0cab31cdb7571ce74c93485aaec2cfaab01
Namjin-kimm <namjin-kim@daum.net>
2026-02-06 18:47:49 +0900

    refactor: null 객체 생성 방지

--- apps/commerce-api/src/main/java/com/loopers/application/user/UserInfo.java
+++ apps/commerce-api/src/main/java/com/loopers/application/user/UserInfo.java
@@ -3,6 +3,7 @@ package com.loopers.application.user;
 import com.loopers.domain.user.UserModel;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public record UserInfo(
     Long id,
@@ -12,6 +13,7 @@ public record UserInfo(
     String email
 ) {
     public static UserInfo from(UserModel userModel) {
+        Objects.requireNonNull(userModel, "userModel은 null일 수 없습니다.");
         return new UserInfo(
             userModel.getId(),
             userModel.getLoginId(),
